0bac8c2a718ef8e1730a55227e0a3b4c992b05ae
Ken Kurosaki <ken.kurosaki1991@gmail.com>
2021-05-18 12:07:35 +0900

    Added a temp title

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5 import QtGui, QtCore
+from PyQt5 import QtGui, QtCore, QtWidgets
 from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWidget, QFileDialog, QGridLayout
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtGui import QCursor
@@ -15,6 +15,26 @@ window.setStyleSheet("background: purple;")
 
 grid = QGridLayout()
 
+#make temporary logo/intro title
+tempTitle = QtWidgets.QLabel(window)
+tempTitle.setText('Do Nothing Quiz!')
+tempTitle.move(90, 100)
+tempTitle.setStyleSheet("font-size: 45px;"
+    + "color: 'white'"
+)
+window.show()
+
+#make button 
+button = QPushButton("ENTER")
+#indicate it is clickable
+button.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+button.setStyleSheet(
+    "border: 5px solid 'orange';"
+    + "font-size: 60px;"
+    + "color: 'white'"
+)
+#place Widgets on grid (Item, Row, Column)
+grid.addWidget(button, 1, 0)
 
 #apply grind to window
 window.setLayout(grid)
@@ -23,10 +43,12 @@ window.show()
 sys.exit(app.exec())
 
 
-#display Logo. Move up once I have a logo
+#display Logo. Move up once I have a logo. Place between grid = QGridLayout() and between window.setLayout(grid)
 #image = QPixmap("LOGO THING GOES HERE")
 #logo = QLabel() 
 #logo.setPixmap(image)
+#logo(QtCore.Qt.AlignCenter)
+#logo.setStyleSheet("margin-top: 50px;")
 
 #Place logo on grid
 #grid.addWidget(logo, 0, 0)
